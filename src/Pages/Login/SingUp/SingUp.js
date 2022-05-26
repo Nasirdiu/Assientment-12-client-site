cd6dcd7a75ec8688acfa91887235b91b427f4e06
nasirdiu <nasir.joypurhat@gmail.com>
2022-05-26 15:04:23 +0600

    color

--- src/Pages/Login/SingUp/SingUp.js
+++ src/Pages/Login/SingUp/SingUp.js
@@ -49,7 +49,7 @@ const SingUp = () => {
     toast("Sing Up done");
   };
   return (
-    <div className=" bg-slate-700 flex justify-center items-center h-screen bg-gray-300	">
+    <div className=" bg-slate-700 flex justify-center items-center h-screen	">
       <div className="card w-96 bg-base-100 shadow-xl">
         <div className="card-body">
           <h2 className="text-center text-2xl font-bold">Sing Up</h2>
